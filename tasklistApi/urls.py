8a1b2a4aef8f88e0b72f35152251057d0049dd06
vfloressduoc <val.floress@duocuc.cl>
2024-07-09 21:11:41 -0400

    updates

--- tasklistApi/urls.py
+++ tasklistApi/urls.py
@@ -18,5 +18,4 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls'))]
+    path('admin/', admin.site.urls)]
